8c98886769935d6399b215801afe4c1f6d9e8ac4
vinceMeens <v.w.a.meens@comcordis.com>
2014-09-19 12:50:42 +0200

    Added test file upload

--- initialize.php
+++ initialize.php
@@ -30,7 +30,6 @@ switch (XXX_PHP::$executionEnvironment)
 				set the gzip-encoded header for your chosen extensions ('Content-encoding: gzip')
 				set the appropriate MIME type: text/javascript or text/css
 				
-				
 				After some digging around I learned that you cannot send compressed javascripts to Safari with the extension of “gz”. It must be “jgz”
 				
 				Mobile: Mozilla/5.0 (iPhone; CPU iPhone OS 6_0_1 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko; Google Page Speed Insights) Version/6.0 Mobile/10A525 Safari/8536.25
